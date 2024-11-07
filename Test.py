98ec64139bcd25eb3007a75087728911a4b25e02
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-07 20:05:48 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,19 +7,23 @@ import numpy as np
 import math
 import base64
 import warnings
+import os
 
 # Suppress TensorFlow warnings
 warnings.filterwarnings("ignore", category=UserWarning, message="No training configuration found in the save file")
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier(
-    "path/to/your/keras_model.h5",
-    "path/to/your/labels.txt"
-)
+
+# Define the path for the model dynamically based on the environment
+# You can either use absolute paths or relative paths based on where your app is running
+model_path = os.environ.get('MODEL_PATH', 'Model/keras_model.h5')  # Default to 'Model/keras_model.h5' if not set
+labels_path = os.environ.get('LABELS_PATH', 'Model/labels.txt')  # Default to 'Model/labels.txt' if not set
+
+# Use environment variables for Heroku or local paths
+classifier = Classifier(model_path, labels_path)
 
 offset = 20
 imgSize = 300
@@ -31,23 +35,31 @@ def translate_image(img):
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
+
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        if h / w > 1:
+        aspectRatio = h / w
+
+        if aspectRatio > 1:
             k = imgSize / h
             wCal = math.ceil(k * w)
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            if imgCrop.size > 0:
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            else:
+                return ''
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
-        prediction, index = classifier.getPrediction(imgWhite, draw=False)
         translation = labels[index]
     else:
         translation = ''
@@ -61,6 +73,7 @@ def translate_asl():
         return jsonify({'img': '', 'translation': ''})
 
     translation = translate_image(img)
+
     _, buffer = cv2.imencode('.jpg', img)
     img_str = base64.b64encode(buffer).decode('utf-8')
 
