1398130328c4ac89ba4d0e3453b63966260f0585
Jeydi0215 <133236586+Jeydi0215@users.noreply.github.com>
2024-11-08 13:50:11 +0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -113,4 +113,4 @@ def proxy():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
